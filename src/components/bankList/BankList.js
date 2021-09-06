c79b86bb0427060bd9c2aafd4a6cf25a723827e3
Nargiza <saparbaeva.nargiza01@gmail.com>
2021-09-06 18:31:01 +0600

    adding css folders

--- src/components/bankList/BankList.js
+++ src/components/bankList/BankList.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import './bankiList.css;
+import classes from  './bankiList.module.css'
 // import '../Image'
 
 class BankList extends React.Component {
     render() {
         return (
            
-                <div className="container-fon d-flex text-center text-white bg-dark">
+                <div className={`${classes.container_fon} d-flex text-center text-white bg-dark`}>
                     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-                        <main id="credits" className="px-3">
+                        <main  className={`${classes.credits} px-3`}>
                             <h1>Мастер подбора кредитов</h1>
                             <p className="fs-5">Персональный подбор кредита на любые цели и отправка заявки в банк</p>
                             <button className="btn btn-outline-light" onClick="document.getElementById('container').scrollIntoView();">Подобрать</button>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../header/header.css'
+import './header.module.css'
 import logo from '..///.///./././../Image/logo.svg'
-
+import classes from './header.module.css'
 
 
 class Header extends React.Component{
   render(){
       return(
-		<div className="header">
-		<div className="navigation">
+		<div className={classes.header}>
+		<div className={classes.navigation}>
 			<div className="row align-items-center">
 				<div className="col mx-3">
 					<a href="#"><img  src={logo} className="px-2" alt="logo"/></a>

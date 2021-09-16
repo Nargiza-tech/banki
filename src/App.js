ad9e82d016727604145f7f79f1c1fdd65bb07532
Nargiza <saparbaeva.nargiza01@gmail.com>
2021-09-16 14:19:56 +0600

    adding addButton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 import Header from './components/header/Header';
 import BankList from './components/bankList/BankList';
 import Footer from "./components/footer/footer";
 import Amount from './components/amount/Amount';
 import Service from './components/service/Service';
 import Partners from './components/partners/Partners';
+import Admin from "./components/admin/Admin";
+
+
+
+
+// import AdminLists from "./components/adminLists/AdminLists";
 
 function App() {
   return (
     <>
     <Header/>
-    <BankList/>
-    <Amount/>
-    <Service/>
-    <Partners/>
-    <Footer/>
-    
+      <Admin/>
+    {/*<BankList/>*/}
+    {/*<Amount/>*/}
+    {/*<Service/>*/}
+    {/*<Partners/>*/}
+    {/*<Footer/>*/}
+
+
 
 </>
   );
